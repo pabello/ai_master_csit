6dcbec4ec36c2235a3148a5e42671925f79fdd6d
Paweł Wacławiak <p.waclawiak@samorzad.p.lodz.pl>
2023-01-15 11:48:59 +0100

    Changing to get results for all cases

--- lab01_salesman/main.py
+++ lab01_salesman/main.py
@@ -6,12 +6,16 @@ from greedy_search import greedy_search
 from heuristics import heuristic_search
 
 
-# seed(6)ssss
-dimmension = 9
+# seed(6)
+max_cities = 9
+for dimmension in range(5, max_cities + 1):
+    cities = utils.generate_cities(dimmension)
 
-cities = utils.generate_cities(dimmension)
-graph = utils.get_symmetric_graph(cities)
+    fc_sym_graph = utils.get_symmetric_graph(cities)
+    fc_asym_graph = utils.get_asymmetric_graph(fc_asym_graph, cities)
+    nfc_sym_graph = utils.remove_connections(fc_sym_graph)
+    nfc_asym_graph = utils.remove_connections(fc_asym_graph)
 
-print(brute_force(graph))
-print(greedy_search(graph))
-print(heuristic_search(graph))
+    print(brute_force(graph))
+    print(greedy_search(graph))
+    print(heuristic_search(graph))
