abc919df354df31066bd281d3d6f3063e5fce4b8
Paweł Wacławiak <p.waclawiak@samorzad.p.lodz.pl>
2023-01-21 11:27:56 +0100

    Prepared for giving extensive results

--- lab01_salesman/main.py
+++ lab01_salesman/main.py
@@ -7,15 +7,32 @@ from heuristics import heuristic_search
 
 
 # seed(6)
-max_cities = 9
+max_cities = 5
 for dimmension in range(5, max_cities + 1):
     cities = utils.generate_cities(dimmension)
 
     fc_sym_graph = utils.get_symmetric_graph(cities)
-    fc_asym_graph = utils.get_asymmetric_graph(fc_asym_graph, cities)
+    fc_asym_graph = utils.get_asymmetric_graph(fc_sym_graph, cities)
     nfc_sym_graph = utils.remove_connections(fc_sym_graph)
     nfc_asym_graph = utils.remove_connections(fc_asym_graph)
 
-    print(brute_force(graph))
-    print(greedy_search(graph))
-    print(heuristic_search(graph))
+    graphs = ((fc_sym_graph, "SYM", "FC"),
+              (fc_asym_graph, "ASYM", "FC"),
+              (nfc_sym_graph, "SYM", "NFC"),
+              (nfc_asym_graph, "ASYM", "NFC"),)
+
+    for graph in graphs:
+        print(graph[1:])
+        bf = brute_force(graph[0])
+        gs = greedy_search(graph[0])
+        hs = heuristic_search(graph[0])
+
+        with open("results.md", "a") as file:
+            file.write(f"| {dimmension} | {graph[1]} | {graph[2]} | Brute force DFS | {bf.get('dfs')[0]} | {bf.get('dfs')[1]} |\n")
+            file.write(f"| {dimmension} | {graph[1]} | {graph[2]} | Brute force BFS | {bf.get('bfs')[0]} | {bf.get('bfs')[1]} |\n")
+            file.write(f"| {dimmension} | {graph[1]} | {graph[2]} | Greedy search | {gs[0]} | {gs[1]} |\n")
+            file.write(f"| {dimmension} | {graph[1]} | {graph[2]} | A* AVG | {hs.get('avg')[0]} | {hs.get('avg')[1]} |\n")
+            file.write(f"| {dimmension} | {graph[1]} | {graph[2]} | A* MIN | {hs.get('min')[0]} | {hs.get('min')[1]} |\n")
+    
+    with open("results.md", "a") as file:
+        file.write("|||||||\n")
